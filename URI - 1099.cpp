c4a3a720189c3841a175875367228bcdada2b61d
Adriel Araujo <araujo.contato021@gmail.com>
2019-01-27 00:46:08 -0300

    Add files via upload

---
+++ URI - 1099.cpp
@@ -0,0 +1,48 @@
+/*Autor: Adriel Araujo
+-Estudante de Ciencias da Computação UECE 2016.2 - 1422340
+Author: Adriel Araujo
+-Student of Computer Science in UECE 2016.2 - 1422340*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a, i, o, x, y, sum;
+	scanf("%d", &a);
+	
+	for(i=0; i<a; i++){
+		
+		sum = 0;
+		scanf("%d %d", &x, &y);
+		
+		if(x == y)
+			printf("%d\n", sum);
+			
+		else if(x > y){
+		
+			for(o=y+1; o<x; o++){
+				if(o%2 == 1|| o%2 == -1){
+					sum += o;
+				}				
+			}
+			printf("%d\n", sum);
+			
+		}
+		
+		else if(y > x){
+		
+			for(o=x+1; o<y; o++){
+				if(o%2 == 1|| o%2 == -1){
+					sum += o;
+				}				
+			}	
+			printf("%d\n", sum);
+			
+		}
+		
+	}
+	
+return 0;
+}
