755040e00fe1b18f57aa0edf3ab21c4b84f42f91
Adriel Araujo <araujo.contato021@gmail.com>
2019-02-07 23:50:04 -0300

    Create URI - 1001 - Solutions.cpp

---
+++ URI - 1001 - Solutions.cpp
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////////
+//                  Autor/Author: Adriel de Oliveira Araujo                     //
+//           Universidade Estadual do Ceará / Ceara State University            //
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main (){
+	
+	int A, B, X;
+	
+	scanf("%d", &A);
+	scanf("%d", &B);
+	
+	printf("X = %d\n", X= (A+B));
+	
+return 0;
+}
