86c432223a7a988ff35ee9c6f2727024c0ea3cdb
Adriel Araujo <araujo.contato021@gmail.com>
2019-02-07 23:53:05 -0300

    Create URI - 1005 - Solution.c

---
+++ URI - 1005 - Solution.c
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////////
+//                  Autor/Author: Adriel de Oliveira Araujo                     //
+//           Universidade Estadual do Ceará / Ceara State University            //
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main (){
+	
+	double A, B, MEDIA;
+	
+	scanf("%lf", &A);
+	scanf("%lf", &B);
+	
+	printf("MEDIA = %.5lf\n", MEDIA=(((A*3.5)+(B*7.5))/11));
+	
+return 0;
+}
