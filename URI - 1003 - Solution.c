8a0b042f7528bd38e87f33a1fb44d1d66d6d00e7
Adriel Araujo <araujo.contato021@gmail.com>
2019-02-07 23:51:36 -0300

    Create URI - 1003 - Solution.c

---
+++ URI - 1003 - Solution.c
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////////
+//                  Autor/Author: Adriel de Oliveira Araujo                     //
+//           Universidade Estadual do Ceará / Ceara State University            //
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main (){
+	
+	int A, B, SOMA;
+	
+	scanf("%d", &A);
+	scanf("%d", &B);
+	
+	printf("SOMA = %d\n", SOMA=(A+B));
+	
+return 0;
+}
