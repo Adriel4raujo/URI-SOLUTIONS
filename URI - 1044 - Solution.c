983f9f71516210d52f53f5fec82a71a56767478b
Adriel Araujo <araujo.contato021@gmail.com>
2019-02-09 13:53:18 -0300

    Create URI - 1044 - Solution.c

---
+++ URI - 1044 - Solution.c
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////////////////////
+//                  Autor/Author: Adriel de Oliveira Araujo                     //
+//           Universidade Estadual do Ceará / Ceara State University            //
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main (){
+	
+	int A, B;
+	
+	scanf("%d %d", &A, &B);
+	
+	if((A%B==0)||(B%A==0)){
+		printf("Sao Multiplos\n");
+	}else{
+		printf("Nao sao Multiplos\n");
+	}
+	
+return 0;
+}
