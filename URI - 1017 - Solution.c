eb98b26b10b2044d30d7c5a399071b18136f74ee
Adriel Araujo <araujo.contato021@gmail.com>
2019-02-09 13:43:51 -0300

    Rename URI - 1002 - Solution.c to URI - 1017 - Solution.c

---
+++ URI - 1017 - Solution.c
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////////////////////////////////
+//                  Autor/Author: Adriel de Oliveira Araujo                     //
+//           Universidade Estadual do Ceará / Ceara State University            //
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main (){
+	
+	int X, Y, Z;
+	
+	double W;
+	
+	scanf("%d %d", &X, &Y);
+	
+	Z= X*Y;
+	
+	W= Z/12.0;
+	
+	printf("%.3lf\n", W);
+	
+return 0;
+}
