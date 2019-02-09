0dc893def0125c7029101cd703a8b4245ce92912
Adriel Araujo <araujo.contato021@gmail.com>
2019-02-09 13:42:55 -0300

    Create URI - 1016 - Solution.c

---
+++ URI - 1016 - Solution.c
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////////////////////////
+//                  Autor/Author: Adriel de Oliveira Araujo                     //
+//           Universidade Estadual do Ceará / Ceara State University            //
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main (){
+	
+	int X, Y;
+	
+	scanf("%d", &X);
+	
+	Y= X*2;
+	
+	printf("%d minutos\n", Y);
+	
+return 0;
+}
