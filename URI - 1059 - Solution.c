eb1f3ef9f28a35b6962144e9d1d7bb9113848202
Adriel Araujo <araujo.contato021@gmail.com>
2019-02-12 14:13:04 -0300

    Create URI - 1059 - Solution.c

---
+++ URI - 1059 - Solution.c
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////////
+//                  Autor/Author: Adriel de Oliveira Araujo                     //
+//           Universidade Estadual do Ceará / Ceara State University            //
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main (){
+	
+	int i, x;
+
+	for(i=1; i<51; i++){
+		x=i*2;
+		printf("%d\n", x);
+	}
+	
+return 0;
+}
